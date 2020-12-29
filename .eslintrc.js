534f1182cda3fad817e719240281a80c45be8705
MrWaip <45konstantin2@gmail.com>
2020-12-29 23:06:14 +0800

    Test husy

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'no-unused-vars': 'error',
+
+    // Allow untyped functions
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    // We will use TypeScript's types for component props instead
+    'react/prop-types': 'off',
+
+    // No need to import React when using Next.js
+    'react/react-in-jsx-scope': 'off',
+
+    // This rule is not compatible with Next.js's <Link /> components
+    'jsx-a11y/anchor-is-valid': 'off',
+
+    // Why would you want unused vars?
+    '@typescript-eslint/no-unused-vars': ['error'],
+
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Includes .prettierrc.js rules
+  },
+  globals: {
+    React: 'writable',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts'],
+      },
+    },
+  },
+};
